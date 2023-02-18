00e3eb9c55483f831efd36dd7006ba759599626d
Panashe Rusakaniko <panasherussy10@gmail.com>
2023-02-18 02:56:20 +0200

    Fetch API

---
+++ week05/scripts/weather.js
@@ -0,0 +1,35 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const cityName = "Trier";
+const apiKey = "c1a5eef8070a8f674335f5d6c0e2a698";
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier&units=imperial&appid=9861e23dd12f9dc56e7581040f23ca48';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      // displayData(data); // uncomment when ready
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayData(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
